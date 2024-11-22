702172387dee5d38655b141527c0768086f27b08
chuboi <favoronyechere@gmail.com>
2024-11-22 19:12:16 +0100

    made changes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var nextConfig = {
-    images: {
-        domains: ["i.pravatar.cc"],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+
+const nextConfig = {
+  images: {
+    domains: ["i.pravatar.cc"],
+  },
+  eslint: {
+
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
 };
-exports.default = nextConfig;
+
+module.exports = nextConfig;
